5fc3fbc18a9e9e2a229427c62e88aa2e7d1f35ca
opi6opi6 <opi6@hanmail.net>
2023-07-25 15:52:18 +0900

    asefea

--- scripts/content.js
+++ scripts/content.js
@@ -1,15 +1,11 @@
 // alert("'ctrl + space'로 실행버튼을 누를 수 있어요");
-window.addEventListener("keydown", async function(e) {
+window.addEventListener("keydown", function(e) {
     if (e.ctrlKey) {
         // console.log("누름")
         // console.log(e.code == "Space");
         if (e.code == "Space") {
-            var status = await chrome.storage.sync.get(["isOn"]);
-            // console.log("status:", status);
-            if (status.isOn) {
             var 실행버튼 = document.querySelector("#run-code");
             실행버튼.click();
-            }
         }
     }
 })
